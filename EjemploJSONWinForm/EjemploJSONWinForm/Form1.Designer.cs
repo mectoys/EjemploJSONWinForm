a84ebf70447ab314c6c27fb81d08a4efe8aad94c
JOSE ALFREDO PONCIANO CARRANZA <mectoys2013@gmail.com>
2018-09-26 16:26:53 -0500

    Ejemplos simples
    Serializar-Deserializar

---
+++ EjemploJSONWinForm/EjemploJSONWinForm/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace EjemploJSONWinForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.listBox4 = new System.Windows.Forms.ListBox();
+            this.listBox5 = new System.Windows.Forms.ListBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(3, 3);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(589, 507);
+            this.tabControl1.TabIndex = 2;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.listBox2);
+            this.tabPage1.Controls.Add(this.listBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(581, 481);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.button1);
+            this.tabPage2.Controls.Add(this.textBox1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(581, 481);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.listBox5);
+            this.tabPage3.Controls.Add(this.listBox4);
+            this.tabPage3.Controls.Add(this.listBox3);
+            this.tabPage3.Controls.Add(this.button2);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(581, 481);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "tabPage3";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(67, 271);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(410, 147);
+            this.listBox2.TabIndex = 3;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(67, 70);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(410, 173);
+            this.listBox1.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(53, 188);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(437, 232);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(142, 33);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(287, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Serializar un objeto dinámico (ExpandoObject) a JSON";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(105, 38);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(365, 23);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Deserializar una cadena de JSON a una clase parcial";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(92, 77);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(378, 95);
+            this.listBox3.TabIndex = 1;
+            // 
+            // listBox4
+            // 
+            this.listBox4.FormattingEnabled = true;
+            this.listBox4.Location = new System.Drawing.Point(92, 195);
+            this.listBox4.Name = "listBox4";
+            this.listBox4.Size = new System.Drawing.Size(378, 95);
+            this.listBox4.TabIndex = 2;
+            // 
+            // listBox5
+            // 
+            this.listBox5.FormattingEnabled = true;
+            this.listBox5.Location = new System.Drawing.Point(92, 362);
+            this.listBox5.Name = "listBox5";
+            this.listBox5.Size = new System.Drawing.Size(378, 95);
+            this.listBox5.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(590, 494);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.tabPage3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox5;
+        private System.Windows.Forms.ListBox listBox4;
+        private System.Windows.Forms.ListBox listBox3;
+    }
+}
+
---
+++ EjemploJSONWinForm/EjemploJSONWinForm/Form1.cs
@@ -0,0 +1,156 @@
+﻿
+
+namespace EjemploJSONWinForm
+{
+    using System;
+    using System.Windows.Forms;
+    using System.IO;
+    using Newtonsoft.Json;
+    using System.Dynamic;
+    using System.Collections.Generic;
+    using Newtonsoft.Json.Linq;
+    using System.Linq;
+    /// <summary>
+    /// https://es.ourcodeworld.com/articulos/leer/54/como-manipular-y-usar-json-con-c-en-winforms
+    /// </summary>
+    public partial class Form1 : Form
+    {
+        string Valor1, Valor2;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dynamic myObject = new ExpandoObject();
+
+            myObject.nombre = "Our Code World";
+            myObject.sitioweb = "http://ourcodeworld.com";
+            myObject.lenguaje = "es-CO";
+            
+            List<string> articulos = new List<string>();
+            articulos.Add("Como usar JSON con C#");
+            articulos.Add("Top 5: Mejores calendarios jQuery");
+            articulos.Add("Otro articulo ... ");
+
+            myObject.articulos = articulos;
+
+            string json = JsonConvert.SerializeObject(myObject);
+
+            //  Console.WriteLine(json);
+            textBox1.Text = json;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string googleSearchText = @"{
+                'responseData': {
+                'results': [
+                 {
+                 'GsearchResultClass': 'GwebSearch',
+                 'unescapedUrl': 'http://en.wikipedia.org/wiki/Paris_Hilton',
+                 'url': 'http://en.wikipedia.org/wiki/Paris_Hilton',
+                 'visibleUrl': 'en.wikipedia.org',
+                 'cacheUrl': 'http://www.google.com/search?q=cache:TwrPfhd22hYJ:en.wikipedia.org',
+                 'title': '<b>Paris Hilton</b> - Wikipedia, the free encyclopedia',
+                 'titleNoFormatting': 'Paris Hilton - Wikipedia, the free encyclopedia',
+                 'content': '[1] In 2006, she released her debut album...'
+                 },
+                 {
+                 'GsearchResultClass': 'GwebSearch',
+                 'unescapedUrl': 'http://www.imdb.com/name/nm0385296/',
+                 'url': 'http://www.imdb.com/name/nm0385296/',
+                 'visibleUrl': 'www.imdb.com',
+                 'cacheUrl': 'http://www.google.com/search?q=cache:1i34KkqnsooJ:www.imdb.com',
+                 'title': '<b>Paris Hilton</b>',
+                 'titleNoFormatting': 'Paris Hilton',
+                 'content': 'Self: Zoolander. Socialite <b>Paris Hilton</b>...'
+                 }
+                ],
+                'cursor': {
+                 'pages': [
+                 {
+                 'start': '0',
+                 'label': 1
+                 },
+                 {
+                 'start': '4',
+                 'label': 2
+                 },
+                 {
+                 'start': '8',
+                 'label': 3
+                 },
+                 {
+                 'start': '12',
+                 'label': 4
+                 }
+                 ],
+                 'estimatedResultCount': '59600000',
+                 'currentPageIndex': 0,
+                 'moreResultsUrl': 'http://www.google.com/search?oe=utf8&ie=utf8...'
+                }
+                },
+                'responseDetails': null,
+                'responseStatus': 200
+                }";
+
+            JObject googleSearch = JObject.Parse(googleSearchText);
+
+            // Obtener la propiedades result en una lista 
+            IList<JToken> results = googleSearch["responseData"]["results"].Children().ToList();
+
+            // Serializa resultados JSON a un objeto .NET
+            IList<SearchResult> searchResults = new List<SearchResult>();
+
+            foreach (JToken result in results)
+            {
+                SearchResult searchResult = JsonConvert.DeserializeObject<SearchResult>(result.ToString());
+                searchResults.Add(searchResult);
+            }
+
+            // Lista las propiedades del resultado de la busqueda (IList)
+            foreach (SearchResult item in searchResults)
+            {
+                // Console.WriteLine(item.Title);
+                //  Console.WriteLine(item.Content);
+                //Console.WriteLine(item.Url);
+                listBox3.Items.Add(item.Title);
+                listBox4.Items.Add(item.Content);
+                listBox5.Items.Add(item.Url);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string json = @"{
+                           'CPU': 'Intel',
+                           'PSU': '500W',
+                           'Drives': [
+                             'DVD read/writer'
+                             /*(broken)*/,
+                             '500 gigabyte hard drive',
+                             '200 gigabype hard drive'
+                                   ]
+                                }";
+
+            JsonTextReader reader = new JsonTextReader(new StringReader(json));
+            while (reader.Read())
+            {
+                if (reader.Value != null)
+                {
+                    Valor1 = string.Format("Token: {0}, Valor: {1}", reader.TokenType, reader.Value);                  
+
+                    listBox1.Items.Add(Valor1);
+                }
+                else
+                {
+                    Valor2 = string.Format("Token: {0}", reader.TokenType);
+                   listBox2.Items.Add(Valor2);
+                }
+            }
+        }
+    }
+}
